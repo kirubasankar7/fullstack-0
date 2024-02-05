08bd39e2572b83fba3534ca5bafd5919d33f6244
Kirubasankar <113581062+kirubasankar7@users.noreply.github.com>
2024-02-05 16:21:41 +0530

    Add files via upload

---
+++ day5/src/pages/pagessidebar/returngifts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../assets/css/pagessidebar/returngifts.css'
+const Returngifts = () => {
+  return (
+    <div className="returngifts-container">
+      <div className="panel">Divine</div>
+      <div className="panel">Favor Bags</div>
+      <div className="panel">Home Decor</div>
+    </div>
+  );
+};
+
+export default Returngifts;
